092cbe8b1d0f267fec61da4e80b5ad6a7354511e
anastasiya-molchkova <amolchkova@mail.ru>
2021-02-22 22:11:47 +0700

    I have generated .exe file to check how programm works and have made some corrections in code in order to receive the right results

--- Data_types/types-check.cpp
+++ Data_types/types-check.cpp
@@ -13,20 +13,22 @@ int main()
 	short shortNumber = -32768;
 	cout << "Short number: " << shortNumber << "\n";
 
-	long Number = -2147483648;
+	long longNumber = -2147483648;
 	cout << "Long number: " << longNumber << "\n";
 
 	unsigned long long uLongLongNumber = 18446744073709551615;
 	cout << "Unsigned long long number: " << uLongLongNumber << "\n";
 
-	int8_t fixInt1byteNumber = -128;
+	int8_t fixInt1byteNumber = 65;
+	// почему-то не работает с отрицательными числами и вообще работает, как char (выводит А)
 	cout << "int8_t number: " << fixInt1byteNumber << "\n";
 
 	uint16_t fixIntUn2byteNumber = 65535;
 	cout << "uint16_t number: " << fixIntUn2byteNumber << "\n";
 
 	char charValue1 = 'n';
-	char charValue2 = -100;
+	char charValue2 = 50;
+        // хотя char по умолчанию signed, он не работает с отрицательными числами
 	cout << "Char value examples: " << charValue1 << " and " << charValue2 << "\n";
 
 	float floatNumber = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862;
