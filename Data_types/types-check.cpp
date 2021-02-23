7347e46d9feb74632b109319f96cd34ae3fd0493
anastasiya-molchkova <amolchkova@mail.ru>
2021-02-23 12:10:39 +0700

    I have accomplished all the home tasks.

--- Data_types/types-check.cpp
+++ Data_types/types-check.cpp
@@ -10,47 +10,75 @@ struct TicTacToeField
 	CellStatus field[field_size][field_size];
 };
 
+// ЗАДАНИЕ 5, структура с битовыми флагами для определения состояний объединения:
+struct UnionCondition
+{
+	union SmallValue
+	{
+		char ch1;
+		unsigned char ch2;
+		int8_t int1;
+		int16_t int2; 
+		short shortInt;  
+	} small_value;
+	bool isChar1 : 1;
+	bool isChar2 : 1;
+	bool isInt1 : 1;
+	bool isInt2 : 1;
+	bool isShort : 1;
+};
+
+// ТОЧКА ВХОДА
 int main()
 {
 	using std::cout;
 
 // ЗАДАНИЕ 1, создание и инициализация переменных основных типов:
 	bool boolValue = false;
-	cout << "Bool value False is: " << boolValue << "\n";
+	cout << "Bool value False is: " << boolValue << ", the size: " << sizeof(bool) << "\n";
 
 	int intNumber = 2147483647;
-	cout << "Integer number: " << intNumber << "\n";
+	cout << "Integer number: " << intNumber << ", the size: " << sizeof(int) << "\n";
 
 	short shortNumber = -32768;
-	cout << "Short number: " << shortNumber << "\n";
+	cout << "Short number: " << shortNumber << ", the size: " << sizeof(short) << "\n";
 
 	long longNumber = -2147483648;
-	cout << "Long number: " << longNumber << "\n";
+	cout << "Long number: " << longNumber << ", the size: " << sizeof(long) << "\n";
 
 	unsigned long long uLongLongNumber = 18446744073709551615;
-	cout << "Unsigned long long number: " << uLongLongNumber << "\n";
+	cout << "Unsigned long long number: " << uLongLongNumber << ", the size: " << sizeof(uLongLongNumber) << "\n";
 
 	int8_t fixInt1byteNumber = 65;
 	// почему-то не работает с отрицательными числами и вообще работает, как char (выводит А)
-	cout << "int8_t number: " << fixInt1byteNumber << "\n";
+	cout << "int8_t number: " << fixInt1byteNumber << ", the size: " << sizeof(int8_t) << "\n";
 
 	uint16_t fixIntUn2byteNumber = 65535;
-	cout << "uint16_t number: " << fixIntUn2byteNumber << "\n";
+	cout << "uint16_t number: " << fixIntUn2byteNumber << ", the size: " << sizeof(uint16_t) << "\n";
 
 	char charValue1 = 'n';
 	char charValue2 = 50;
         // хотя char по умолчанию signed, он не работает с отрицательными числами
-	cout << "Char value examples: " << charValue1 << " and " << charValue2 << "\n";
+	cout << "Char value examples: " << charValue1 << " and " << charValue2 << ", the size: " << sizeof(char) << "\n";
 
 	float floatNumber = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862;
-	cout << "Float number: " << floatNumber << "\n";
+	cout << "Float number: " << floatNumber << ", the size: " << sizeof(float) << "\n";
 
 	double doubleNumber = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862;
-	cout << "Double number: " << doubleNumber << "\n";
+	cout << "Double number: " << doubleNumber << ", the size: " << sizeof(double) << "\n";
 
 	long double longDoubleNumber = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862;
-	cout << "Long double number: " << longDoubleNumber << "\n";
+	cout << "Long double number: " << longDoubleNumber << ", the size: " << sizeof(long double) << "\n";
 
 // ЗАДАНИЕ 3, массив, способный содержать возможные варианты для игры в крестики-нолики:
-	CellStatus array_field[3][3]{{CellStatus::Empty, CellStatus::Empty, CellStatus::Empty}, {CellStatus::Empty, CellStatus::Empty, CellStatus::Empty}, {CellStatus::Empty, CellStatus::Empty, CellStatus::Empty}};
+	CellStatus array_field[3][3]{{CellStatus::Empty, CellStatus::Empty, CellStatus::Empty}, 
+                                     {CellStatus::Empty, CellStatus::Empty, CellStatus::Empty}, 
+                                     {CellStatus::Empty, CellStatus::Empty, CellStatus::Empty}};
+
+// к ЗАДАНИЮ 5, проверка размера структуры с битовыми флагами:
+	UnionCondition sm = {.small_value.shortInt = 651, .isChar1 = false, .isChar2 = false, 
+			     .isInt1 = false, .isInt2 = false, .isShort = true};
+	cout << "Size of structure with union and its conditions: " << sizeof(sm) << "\n";
+
+	return 0;
 }
