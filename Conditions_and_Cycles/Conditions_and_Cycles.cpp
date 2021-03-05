183a97fd60bc1b4f62f5becbd8b21c3217d2918a
anastasiya-molchkova <amolchkova@mail.ru>
2021-03-05 15:03:32 +0700

    All the home tasks ara solved successfully

--- Conditions_and_Cycles/Conditions_and_Cycles.cpp
+++ Conditions_and_Cycles/Conditions_and_Cycles.cpp
@@ -22,5 +22,48 @@ int main()
         sum_check = true;
     std::cout << "The result of sum check is " << std::boolalpha << sum_check << "\n\n";
 
+    // TASK 2:
+    std::cout << "TASK 2:\n";
+    int number3 = 0;
+    std::cout << "Please enter a positive integer number to check for dividers: ";
+    std::cin >> number3;
+    if (number3 <= 3)
+        std::cout << "The number is negative or simple\n\n";
+    else // число не меньше 4 - уже достаточно для нормальной проверки
+    {
+        bool is_simple = true;
+        // понятно, что число всегда делится на 1 и само себя, нам нужно понять, есть ли у него другие делители.
+        // чтобы это понять, достаточно проверить его делители, не превышающие половины этого числа.
+        for (int div = 2; div <= (number3 / 2); ++div)
+            if ((number3 % div) == 0)
+            {
+                is_simple = false;
+                break;                              // если нашли хоть один делитель, выходим из цикла
+            }
+        std::cout << "The number is simple is " << is_simple << "\n\n";
+    }
+
+    // TASK 3:
+    std::cout << "TASK 3:\n";
+    const int ten = 10;
+    bool check_for_ten = false;
+    if ((number1 == ten) || (number2 == ten) || ((number1 + number2) == ten))
+        check_for_ten = true;
+    std::cout << "The result of check " << number1 << " and " << number2 << " for ten is " << check_for_ten << "\n\n";
+
+    // TASK 4:
+    std::cout << "TASK 4:\n";
+    int year = 0;
+    std::cout << "Please enter a year: ";
+    std::cin >> year;
+    bool is_leap_year = false;
+    if (year % 400 == 0)
+        is_leap_year = true;
+    // если не делится на 100 и 400, но делится на 4
+    else if ((year % 100 != 0) && (year % 4 == 0))
+        is_leap_year = true;
+    // иначе не делится на 4 без остатка, остаётся false
+    std::cout << "This year is leap is " << is_leap_year << "\n\n";
+    
     return 0;
 }
