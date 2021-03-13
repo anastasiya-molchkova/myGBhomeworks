6c87b62244c07e2ffcab3df1a8af83d38e160170
anastasiya-molchkova <amolchkova@mail.ru>
2021-03-13 16:33:10 +0700

    Task 2 was done successfully.

--- WorkWithFiles/WorkWithFiles.cpp
+++ WorkWithFiles/WorkWithFiles.cpp
@@ -24,22 +24,58 @@ void writeToFile(const std::string& filename, const std::string& text)
     file.close();
 }
 
-void task1()
+void task1(const std::string& filename1, const std::string& filename2)
 {
-    std::string file1name{ "File1.txt" };
-    std::string file2name{ "File2.txt" };
-    
-    createFile(file1name);
+    createFile(filename1);
     std::string text1{ "In the continuing debate on whether one needs to learn C before C++, I am firmly convinced that it is best to go directly to C++.\n" };
-    writeToFile(file1name, text1);
+    writeToFile(filename1, text1);
 
-    createFile(file2name);
+    createFile(filename2);
     std::string text2{ "C++ is safer and more expressive,and it reduces the need to focus on low - level techniques.\n" };
-    writeToFile(file2name, text2);
+    writeToFile(filename2, text2);
+}
+
+std::string readFromFile(const std::string& filename)
+{
+    std::ifstream file(filename);
+    std::string textFromFile;
+    std::getline(file, textFromFile);
+    std::cout << "Text \""<< textFromFile <<"\" has been read from " << filename << "\n";
+    file.close();
+    return textFromFile;
+}
+
+void task2(const std::string& filename1, const std::string& filename2, const std::string& resultFilename)
+{
+    std::string text1{ readFromFile(filename1) };
+    std::string text2{ readFromFile(filename2) };
+
+    // динамически выделяем память под буферный текст из двух файлов: 
+    unsigned howManyChars = text1.length() + text2.length();
+    char* bufferText = new char[howManyChars + 2];           //  2 - дополнительныые элементы под разделитель двух текстов и терминальный нуль
+
+    // получаем буферный текст из двух
+    for (size_t i = 0; i < text1.length(); ++i)
+        bufferText[i] = text1[i];
+    bufferText[text1.length()] = '\n';                      // чтобы разделить два текста
+    for (size_t i = 0; i < text2.length(); ++i)
+        bufferText[i + text1.length() + 1] = text2[i];
+    bufferText[howManyChars + 1] = '\0';
+
+    createFile(resultFilename);
+    writeToFile(resultFilename, bufferText);
+
+    delete[] bufferText;
 }
 
 int main()
 {
-    task1();
+    std::string filename1{ "File1.txt" };
+    std::string filename2{ "File2.txt" };
+    task1(filename1, filename2);
+
+    std::string resultFile{ "Result.txt" };
+    task2(filename1, filename2, resultFile);
+
     return 0;
 }
