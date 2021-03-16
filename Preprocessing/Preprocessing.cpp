16ea5bd34842210a3166b0cac1bf59d9c216486c
anastasiya-molchkova <amolchkova@mail.ru>
2021-03-16 22:40:03 +0700

    It seems that the first task was solved very easy...

--- Preprocessing/Preprocessing.cpp
+++ Preprocessing/Preprocessing.cpp
@@ -3,8 +3,19 @@
    3* Описать макрокоманду, возвращающую количество элементов локального массива, принимающую на вход ссылку на массив и его тип */
 
 #include <iostream>
+// TASK 1:
+#define ZERO 0
+#define CHECK_LIMIT(NUMB, MAX) ((NUMB >= ZERO && NUMB < MAX)? true : false)
 
 int main()
 {
+    std::cout << "Task 1 check:\n";
+    int number = 323, upper_limit = 100;
+    if (CHECK_LIMIT(number, upper_limit))
+        std::cout << "Number " << number << " fits the range from " << ZERO << " to " << upper_limit << '\n';
+    else 
+        std::cout << "Number " << number << " doesn't fit the range from " << ZERO << " to " << upper_limit << '\n';
+    
+    
     return 0;
 }
