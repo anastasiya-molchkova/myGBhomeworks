33ad689e31714183587cc1e68b2f47db7d212bc7
anastasiya-molchkova <amolchkova@mail.ru>
2021-03-17 17:35:16 +0700

    Task2 was resolved correctly after the lesson, separate functions were made for each task to simplify code reading.

--- Preprocessing/Preprocessing.cpp
+++ Preprocessing/Preprocessing.cpp
@@ -8,39 +8,68 @@
 #define CHECK_LIMIT(NUMB, MAX) ((NUMB >= ZERO && NUMB < MAX)? true : false)
 
 // TASK 2:
-#define GET_ELEMENT(ARR, COLMNS, row, col) (*((&ARR[0][0])+(row*COLMNS)+col))
+#define GET_ELEMENT(ARR, row, col) *(*(ARR + row) + col)
+
+void task1();
+void task2();
+void task3();
 
 int main()
 {
     /////////////////////////// TASK 1 //////////////////////////////////
     std::cout << "Task 1 check:\n";
+    task1();
+
+    /////////////////////////// TASK 2 //////////////////////////////////
+    std::cout << "Task 2 check:\n";
+    task2();
+
+    /////////////////////////// TASK 3 //////////////////////////////////
+    std::cout << "Task 3 check:\n";
+    task3();
+
+    return 0;
+}
+
+void task1()
+{
     int number = 323, upper_limit = 100;
     if (CHECK_LIMIT(number, upper_limit))
         std::cout << "Number " << number << " fits the range from " << ZERO << " to " << upper_limit << '\n';
     else
         std::cout << "Number " << number << " doesn't fit the range from " << ZERO << " to " << upper_limit << "\n\n";
+}
 
-    /////////////////////////// TASK 2 //////////////////////////////////
-    std::cout << "Task 2 check:\n";
+void task2()
+{
     // будем динамически создавать массив NxM:
     const size_t N = 3;
     const size_t M = 4;
     int** array = new int* [N];
     for (size_t i = 0; i < N; ++i)
         array[i] = new int[M];
-    
+
     // заполняем массив NxM, выводим в виде матрицы:
     for (size_t i = 0; i < N; ++i)
         for (size_t j = 0; j < M; ++j)
         {
-            array[i][j] = (i+1) * 10 + j;
+            array[i][j] = (i + 1) * 10 + j;
             std::cout << array[i][j] << " ";
             if (j + 1 == M)
                 std::cout << '\n';
         }
-    
-    size_t x = 1, y = 2;
-    std::cout << "Element [" << x << "][" << y << "] = " << GET_ELEMENT(array, M, x, y) << "\n\n";
 
-    return 0;
+    // проверяем определённый элемент массива с помощью макроса: 
+    size_t x = 1, y = 2;
+    std::cout << "Element [" << x << "][" << y << "] = " << GET_ELEMENT(array, x, y) << "\n\n";
+
+    // освобождаем выделенную под массив память:
+    for (size_t i = 0; i < N; ++i)
+        delete[] array[i];
+    delete[] array;
+}
+
+void task3()
+{
+
 }
