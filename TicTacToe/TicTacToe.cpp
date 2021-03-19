b6b3690a5afd2c0a2235001326a69a181b5d3b69
anastasiya-molchkova <amolchkova@mail.ru>
2021-03-19 23:31:55 +0700

    TicTacToe project was created on a new branch. Initial data were taken from the second homework.

---
+++ TicTacToe/TicTacToe.cpp
@@ -0,0 +1,29 @@
+﻿/* Консольная игра "Крестики-Нолики", но на красивом поле и с искусственным интеллектом ;)  */
+
+#include <iostream>
+
+// возможные состояния клетки игры в крестики-нолики:
+enum class CellStatus{Empty = 0, X, O};
+
+using cellSize = size_t;
+enum class Colors{Green, Red, Yellow, Blue, Black, White};
+
+struct CellParameters
+{
+	Colors color; 
+	cellSize length;
+	cellSize width;
+}
+
+// поле для игры в крестики-нолики:
+struct Field
+{
+	static const short field_size = 3;               // требует именно static!
+	CellParameters cell;
+	CellStatus field[field_size][field_size];
+};
+
+int main()
+{
+    return 0;
+}
