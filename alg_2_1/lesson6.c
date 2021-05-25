b0b7f63457fce3eb4d346d7c1516c1a8bf15800e
anastasiya-molchkova <amolchkova@mail.ru>
2021-05-25 23:44:59 +0700

    Algorithm for the second task was realized.

--- alg_2_1/lesson6.c
+++ alg_2_1/lesson6.c
@@ -1,25 +1,9 @@
 #include <stdio.h>
 #include <locale.h>  // для кириллицы
 #include <stdlib.h>  // для генерации случайных чисел
-
-// вспомогательная функция - получаем целое положительное число от пользователя
-unsigned get_positive_number(const char* text_to_user)
-{
-    char text_number[256];
-    int number = 0;
-    do
-    {
-        printf("%s", text_to_user);
-        scanf("%s", text_number);
-        number = atoi(text_number);
-    } while (number <= 0);
-    return (unsigned)number;
-}
+#include <math.h>    // для sqrt, fabs, pow
 
 ///////////////////////////////// ЗАДАНИЕ 1
-
-#ifndef TASK1
-#define TASK1
 
 // определяем размеры двумерного массива:
 #define N 10    // количество строк двумерного массива
@@ -82,7 +66,7 @@ void bubble_sort(int** array)
 // Решение первого задания - пузырьковая сортировка двумерного массива
 void task1()
 {
-    printf("1. Пузырьковая сортировка двумерного массива.\n");
+    printf("1. Пузырьковая сортировка двумерного массива.\n\n");
 
     int** array = NULL;
     array = init_array(array);
@@ -98,13 +82,89 @@ void task1()
     printf("\n\n");
 }
 
-#endif // !TASK1
+#undef N
+#undef M
+
+///////////////////////////////// ЗАДАНИЕ 2
+
+// получает ненулевое число от пользователя
+float get_number(const char* text_to_user)
+{
+    char text_number[256];
+    int number = 0;
+    do
+    {
+        printf("%s", text_to_user);
+        scanf("%s", text_number);
+        number = atof(text_number);
+    } while (number == 0);
+    return (float)number;
+}
+
+#define ELEVEN 11    // количество чисел в последовательности
+
+// получает элементы массива от пользователя
+void get_array(float* array)
+{
+    for (int i = 0; i < ELEVEN; ++i)
+        array[i] = get_number("Введите ненулевое число: ");
+}
+
+// инвертирует массив
+void invert_array(float* array)
+{
+    for (int i = 0; i < ELEVEN / 2; ++i)
+    {
+        float temp = array[i];
+        array[i] = array[ELEVEN-1-i];
+        array[ELEVEN - 1 - i] = temp;
+    }
+}
+
+// печатает массив
+void print_array(const float* array)
+{
+    for (int i = 0; i < ELEVEN; ++i)
+        printf("%.2f  ", array[i]);
+    printf("\n");
+}
+
+// Решение второго задания - "алгоритм Трабба-Прадо-Кнута"
+void task2()
+{
+    printf("1. Алгоритм Трабба-Прадо-Кнута.\n\n");
+
+    printf("Получение элементов последовательности:\n");
+    float array[ELEVEN] = {};
+    get_array(array);
+    printf("\nПолучена последовательность:\n");
+    print_array(array);
+
+    invert_array(array);
+    printf("Инвертированная последовательность:\n");
+    print_array(array);
+
+    float x;
+    for (int i = 0; i < ELEVEN; ++i)
+    {
+        x = array[i];
+        array[i] = sqrt(fabs(x)) + 5 * pow(x, 3);
+        if (array[i] > 400)
+            printf("Преобразованный  %d-й элемент последовательности %.2f > 400!\n", i+1, array[i]);
+    }
+    printf("\nПреобразованная оследовательность:\n");
+    print_array(array);
+    printf("\n\n");
+}
+
+#undef ELEVEN
 
 int main()
 {
     setlocale(LC_CTYPE, "rus");  // для кириллицы
 
     task1();
+    task2();
 
     return 0;
 }
