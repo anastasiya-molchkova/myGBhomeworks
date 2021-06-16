df187444af377c17e3d7d0143e2b5997c58b3c7f
anastasiya-molchkova <amolchkova@mail.ru>
2021-06-16 22:05:19 +0700

    The second task with shuffle encryption and decryption was done. It works, but the solution is not very clear. I want to clean it and to add some comments a bit later.

--- alg_5_2/cryptography.c
+++ alg_5_2/cryptography.c
@@ -22,7 +22,7 @@ void cycleShift(char* input_arr, char* output_arr, const unsigned int size, cons
 }
 
 // зашифровывает переданное сообщение шифром Цезаря, принимая в качестве параметра ключ-сдвиг
-char* encryption(char* secret_message, int key, char* alphabet, const unsigned int alphabet_size)
+char* cesarEncryption(char* secret_message, int key, char* alphabet, const unsigned int alphabet_size)
 {
     char* alphabet_code = (char*)malloc(alphabet_size * sizeof(char));
     cycleShift(alphabet, alphabet_code, alphabet_size, key);
@@ -31,7 +31,7 @@ char* encryption(char* secret_message, int key, char* alphabet, const unsigned i
     for (message_len = 0; secret_message[message_len] != '\0'; ++message_len);
 
     char* cryptogram = (char*)malloc(message_len * sizeof(char));
-    for (int i = 0; i < message_len; ++i)
+    for (unsigned int i = 0; i < message_len; ++i)
         cryptogram[i] = alphabet_code[secret_message[i] - 'A'];
     cryptogram[message_len] = '\0';
 
@@ -40,7 +40,7 @@ char* encryption(char* secret_message, int key, char* alphabet, const unsigned i
 }
 
 // расшифровывает переданную криптограмму, зашифрованную шифром Цезаря, принимая в качестве параметра ключ-сдвиг
-char* decryption(char* cryptogram, int key, char* alphabet, const unsigned int alphabet_size)
+char* cesarDecryption(char* cryptogram, int key, char* alphabet, const unsigned int alphabet_size)
 {
     char* alphabet_code = (char*)malloc(alphabet_size * sizeof(char));
     cycleShift(alphabet, alphabet_code, alphabet_size, -key);
@@ -49,7 +49,7 @@ char* decryption(char* cryptogram, int key, char* alphabet, const unsigned int a
     for (message_len = 0; cryptogram[message_len] != '\0'; ++message_len);
 
     char* secret_message = (char*)malloc(message_len * sizeof(char));
-    for (int i = 0; i < message_len; ++i)
+    for (unsigned int i = 0; i < message_len; ++i)
         secret_message[i] = alphabet_code[cryptogram[i] - 'A'];
     secret_message[message_len] = '\0';
     
@@ -76,20 +76,152 @@ void task1()
     const int cesar_shift = 3;
 
     printf("Криптограмма:\n");
-    char* cryptogram = encryption(message, cesar_shift, alphabet, alphabet_size);
+    char* cryptogram = cesarEncryption(message, cesar_shift, alphabet, alphabet_size);
     puts(cryptogram);
 
     printf("Расшифровка:\n");
-    char* result = decryption(cryptogram, cesar_shift, alphabet, alphabet_size);
+    char* result = cesarDecryption(cryptogram, cesar_shift, alphabet, alphabet_size);
     puts(result);
+    printf("\n");
 
     free(alphabet);
+}
+
+//////////////////// ЗАДАНИЕ 2
+
+// создаёт 2D массив
+char** init2DArray(int rows, int cols)
+{
+    char** array = (char**)calloc(sizeof(char*), rows);
+    for (int i = 0; i < rows; ++i)
+        *(array + i) = (char*)calloc(sizeof(char), cols);
+    return array;
+}
+
+char get2dChar(char** array, const int row, const int col)
+{
+    return *((*(array + row)) + col);
+}
+
+void set2dChar(char** array, const int row, const int col, char letter)
+{
+    *((*(array + row)) + col) = letter;
+}
+
+// зашифровывает переданное сообщение перестановками по передаваемому ключевому слову
+char* shuffleEncryption(char* secret_message, char* key_word)
+{
+    unsigned int message_len;  // длина сообщения
+    for (message_len = 0; secret_message[message_len] != '\0'; ++message_len);
+
+    unsigned int columns_num;
+    for (columns_num = 0; key_word[columns_num] != '\0'; ++columns_num);
+
+    unsigned int rows_num;
+    rows_num = (message_len % columns_num == 0) ? (message_len / columns_num) : (message_len / columns_num + 1);
+
+    char** initial_array = init2DArray(rows_num, columns_num);
+    for (unsigned int i = 0; i < rows_num; ++i)
+        for (unsigned int j = 0; j < columns_num; ++j)
+            set2dChar(initial_array, i, j, secret_message[i * columns_num + j]);
+
+    char* cryptogram = (char*)malloc(message_len * sizeof(char));
+    unsigned int counter = 0;
+    unsigned int idx = 0;
+    char letter = 'A';
+    while (letter <= 'Z' && counter < columns_num)
+    {
+        for (unsigned int j = 0; j < columns_num; ++j)
+            if (key_word[j] == letter)
+            {
+                for (unsigned int i = 0; i < rows_num; ++i)
+                    cryptogram[idx++] = get2dChar(initial_array, i, j);
+                ++counter;
+                break;
+            }
+        ++letter;
+    }
+    cryptogram[message_len] = '\0';
+
+    for (unsigned int i = 0; i < rows_num; ++i)
+        free(initial_array[i]);
+    free(initial_array);
+
+    return cryptogram;
+}
+
+// расшифровывает переданное сообщение перестановками по передаваемому ключевому слову
+char* shuffleDecryption(char* cryptogram, char* key_word)
+{
+    unsigned int message_len; // длина сообщения
+    for (message_len = 0; cryptogram[message_len] != '\0'; ++message_len);
+
+    unsigned int columns_num;
+    for (columns_num = 0; key_word[columns_num] != '\0'; ++columns_num);
+
+    unsigned int rows_num;
+    rows_num = (message_len % columns_num == 0) ? (message_len / columns_num) : (message_len / columns_num + 1);
+
+    char** shuffle_array = init2DArray(rows_num, columns_num);
+    for (unsigned int i = 0; i < rows_num; ++i)
+        for (unsigned int j = 0; j < columns_num; ++j)
+            set2dChar(shuffle_array, i, j, cryptogram[i + j * rows_num]);
+
+    char* result = (char*)malloc(message_len * sizeof(char));
+    unsigned int counter = 0;
+    char letter = 'A';
+    while (letter <= 'Z' && counter < columns_num)
+    {
+        for (unsigned int j = 0; j < columns_num; ++j)
+            if (key_word[j] == letter)
+            {
+                unsigned int column = 0;
+                while (key_word[column] != letter)
+                    ++column;
+                for (unsigned int i = 0; i < rows_num; ++i)
+                    result[i * columns_num + column] = get2dChar(shuffle_array, i, counter);
+                ++counter;
+                break;
+            }
+        ++letter;
+    }
+    result[message_len] = '\0';
+
+    for (unsigned int i = 0; i < rows_num; ++i)
+        free(shuffle_array[i]);
+    free(shuffle_array);
+
+    return result;
+}
+
+// шифрует строку перестановками по ключевому слову, расшифровывает криптограмму с помощью этого же слова
+void task2()
+{
+    printf("2. Шифрование и расшифровка перестановками\n\n");
+
+    printf("Исходное сообщение:\n");
+    const unsigned int max_message_size = 100;
+    char message[max_message_size] = "THISISASECRETMESSAGEFORSHUFFLEENCRYPTION";
+    puts(message);
+
+    char key_word[] = "BLACK";   // ключевое слово для шифрования и расшифровки
+
+    printf("Криптограмма:\n");
+    char* cryptogram = shuffleEncryption(message, key_word);
+    puts(cryptogram);
+
+    printf("Разгаданное сообщение:\n");
+    char* unraveled_message = shuffleDecryption(cryptogram, key_word);
+    puts(unraveled_message);
+
+    //free(cryptogram);
+    //free(unraveled_message);
 }
 
 int main()
 {
     setlocale(LC_CTYPE, "rus");  // для кириллицы
     task1();
-    //task2();
+    task2();
     return 0;
 }
