a78531e3060d6858b6962f2a921da33febc283bd
anastasiya-molchkova <amolchkova@mail.ru>
2021-11-12 21:28:30 +0700

    Первое задание выполнено. Я бы не сказала, что оно супер-лёгкое, пришлось даже отлаживать.

--- CPP_moments4/CPP_moments4.cpp
+++ CPP_moments4/CPP_moments4.cpp
@@ -1,8 +1,4 @@
 ﻿/* 
-	1. Имеется отсортированный массив целых чисел. Необходимо разработать функцию insert_sorted, 
-	которая принимает вектор и новое число и вставляет новое число в определенную позицию в векторе, 
-	чтобы упорядоченность контейнера сохранялась. Реализуйте шаблонную функцию insert_sorted, 
-	которая сможет аналогично работать с любым контейнером, содержащим любой тип значения.
 	2. Сгенерируйте вектор, состоящий из 100 вещественный чисел, представляющий собой значения аналогового сигнала. 
 	На основе этого массива чисел создайте другой вектор целых чисел, представляющий цифровой сигнал, 
 	в котором будут откинуты дробные части чисел. Выведите получившиеся массивы чисел. 
@@ -16,8 +12,57 @@
 */
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~TASK-1~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Имеется отсортированный массив целых чисел.Необходимо разработать функцию insert_sorted,
+// которая принимает вектор и новое число и вставляет новое число в определенную позицию в векторе,
+// чтобы упорядоченность контейнера сохранялась. Реализуйте шаблонную функцию insert_sorted,
+// которая сможет аналогично работать с любым контейнером, содержащим любой тип значения.
+
+template<typename T>
+void insert_sorted(std::vector<T>& sorted_vec, const T new_value, bool asc_order = true)
+{
+	if (asc_order) 
+		sorted_vec.insert(std::upper_bound(sorted_vec.begin(), sorted_vec.end(), new_value), new_value);
+	else
+	{
+		auto pos_to_ins = std::upper_bound(sorted_vec.rbegin(), sorted_vec.rend(), new_value);
+		int shift = sorted_vec.rend() - pos_to_ins;
+		sorted_vec.insert(sorted_vec.begin() + shift, new_value);
+	}
+}
+
+void task1()
+{
+	std::cout << "TASK 1\n";
+	const size_t count = 35;
+	std::vector<int> numbers(count);
+	std::generate(numbers.begin(), numbers.end(), []() {return (rand() % 100); });
+
+	// тестируем для вектора, отсортированного по возрастанию:
+	std::sort(numbers.begin(), numbers.end());
+	std::cout << "Initial vector:\n";
+	std::copy(numbers.begin(), numbers.end(), std::ostream_iterator<int>{std::cout, " "});
+	int value_to_insert = 72;
+	std::cout << "\nResult vector after insertion " << value_to_insert << ":\n";
+	insert_sorted(numbers, value_to_insert);
+	std::copy(numbers.begin(), numbers.end(), std::ostream_iterator<int>{std::cout, " "});
+
+	// тестируем для вектора, отсортированного по убыванию:
+	std::cout << "\n\nInitial vector with descending order to sort:\n";
+	std::reverse(numbers.begin(), numbers.end());
+	std::copy(numbers.begin(), numbers.end(), std::ostream_iterator<int>{std::cout, " "});
+	value_to_insert = 91;
+	std::cout << "\nResult vector after insertion " << value_to_insert << ":\n";
+	insert_sorted(numbers, value_to_insert, false);
+	std::copy(numbers.begin(), numbers.end(), std::ostream_iterator<int>{std::cout, " "});
+	std::cout << "\n" << std::endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	task1();
+	return 0;
 }
